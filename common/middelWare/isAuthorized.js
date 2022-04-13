6bc80e7d29d40e13622fff84e99d1b66865070a5
Abdelrahman-88 <abdo.elsaid@hotmail.com>
2022-04-13 13:50:40 +0200

    make it better

--- common/middelWare/isAuthorized.js
+++ common/middelWare/isAuthorized.js
@@ -4,14 +4,12 @@ const rbac = require('../rbac/rbac');
 const User = require('../../src/users/model/user.model');
 
 module.exports = (endPoint) => {
-    console.log('err');
     return async(req, res, next) => {
         try {
             if (req.headers.authorization) {
                 const token = req.headers.authorization.split(" ")[1]
                 if (token) {
                     const decoded = jwt.verify(token, process.env.SECRET_KEY)
-                    console.log(decoded);
                     const user = await User.findOne({ _id: decoded._id })
                     if (user) {
                         const isAllowed = await rbac.can(user.role, endPoint)
@@ -19,20 +17,20 @@ module.exports = (endPoint) => {
                             req.user = user
                             next()
                         } else {
-                            res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED1" })
+                            res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED" })
                         }
                     } else {
-                        res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED2" })
+                        res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED" })
                     }
                 } else {
-                    res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED3" })
+                    res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED" })
                 }
 
             } else {
-                res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED4" })
+                res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED" })
             }
         } catch (error) {
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "UNAUTHORIZED5" })
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "UNAUTHORIZED" })
         }
     }
 }
